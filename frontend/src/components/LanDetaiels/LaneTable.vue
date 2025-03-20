c791eaf82489814a8095c899449700af8c60662d
Nada-Rjb <enadaragab2020@gmail.com>
2025-03-20 08:32:17 +0200

    finished style

--- frontend/src/components/LanDetaiels/LaneTable.vue
+++ frontend/src/components/LanDetaiels/LaneTable.vue
@@ -5,10 +5,11 @@
         <div class="search-container">
             <table>
                 <tr>
-                    <th>client Name </th>
-                    <th>Invoiced</th>
-                    <th>Payment</th>
-                    <th>Pass by</th>             
+                  <th>اسم العميل</th>
+                  <th>الفاتوره</th>
+                  <th>الدفع</th>
+                  <th>المرور</th>
+            
                 </tr>
                 <tr v-for="(row,rowIndex) in tableData" :key="rowIndex"
                 >
@@ -45,8 +46,8 @@
 th, td {
   text-align: center;
   padding: 0.5rem;
-  border-bottom: 1px solid black; 
-  border-right: 1px solid black; 
+  border-bottom: 2px solid #D9D9D9; 
+  border-right: 2px solid #D9D9D9; 
   border-top: none;
 }
 
@@ -56,15 +57,17 @@ th:last-child, td:last-child {
 th:first-child, td:first-child {
   border-right: none ;
 }
-.success {
+/* .success {
   background-color: lightgreen;
 }
 
 .failed {
   background-color: lightcoral;
-}
+} */
 .status-icon {
     margin: auto;
+    width: 50px;
+    height: 50px;
 }
 
   </style>
