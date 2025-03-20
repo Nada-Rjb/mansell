28389d65f1c15b150025bc1f6b25d7602d218fc3
Nada-Rjb <enadaragab2020@gmail.com>
2025-03-20 05:56:02 +0200

    fished style of state button

--- frontend/src/components/global/StatusButton.vue
+++ frontend/src/components/global/StatusButton.vue
@@ -5,8 +5,8 @@
           <button :class="['status-button', status]">
             <span class="lable">{{ label }}</span>
             <span :class="['status-icon', status]">
-              <span v-if="status === 'success'">✔</span>
-              <span v-else-if="status === 'error'">✖</span>
+              <span v-if="status === 'success'"><img src="../../assets/imges/correctsign.svg" alt=""></span>
+              <span v-else-if="status === 'error'"><img src="../../assets/imges/Wrongsign.svg" alt=""></span>
             </span>
           </button>
         </div>
@@ -29,49 +29,64 @@
   };
   </script>
   <style lang="scss" scoped>
-  .status-button {
- max-width: calc(100% / 3); 
+ .status-button {
+  width: 30%;
   min-width: 90px;
-     height: 40px;
-    background-color: rgb(49, 49, 46);
-    border-radius: var(--border-radius-2);
-    color: #f5f5f5;
-    cursor: pointer;
+  max-width: 200px; 
+  padding: 0.5rem 1rem;
+    height: 40px; 
+
+  @media (min-width: 760px) {
+    width: 25%; 
+    min-width: 120px;
+    max-width: 250px;
+    height: 40px; 
+  }
+
+  @media (min-width: 1024px) {
+    width: 20%;
+    min-width: 140px;
+    max-width: 280px;
+    height: 40px;
+  }
+
+  background-color: rgb(49, 49, 46);
+  border-radius: var(--border-radius-2);
+  color: #f5f5f5;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  font-weight: bold;
+  position: relative;
+
+  &.success {
+    background-color: #14ae5c;
+  }
+
+  &.error {
+    background-color: #ec221f;
+  }
+
+  &.nothing {
+    background-color: red;
+  }
+
+  .status-icon {
+    width: 23px;
+    height: 23px;
+    position: absolute;
+    top: -15px;
+    right: -10px;
     display: flex;
     align-items: center;
     justify-content: center;
-    border: none;
-    font-weight: bold;
-    position: relative;
-    &.success {
-      background-color: #14ae5c;
-    }
-  
-    &.error {
-      background-color: #ec221f;
-    }
-    &.nothing {
-      background-color: red;
-    }
-    .status-icon {
-      width: 23px;
-      height: 23px;
-      position: absolute;
-      top: -15px;
-      right: -10px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      border-radius: 50%;
-      font-size: 14px;
-      &.success {
-        background-color: #c8e6c9;
-        color: #4caf50;
-      }
-      &.error {
-        background-color: #ff9992; /* Light red */
-        color: #ec221f;
-      }
-    }
+    border-radius: 50%;
+    font-size: 14px;
   }
+}
+
+  
+ 
   </style>
