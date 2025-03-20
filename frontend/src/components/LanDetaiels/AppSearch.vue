5e73fdaa56702731d96abca49d44b5cfa458a399
Nada-Rjb <enadaragab2020@gmail.com>
2025-03-20 08:42:55 +0200

    finshed style for nav in lan

--- frontend/src/components/LanDetaiels/AppSearch.vue
+++ frontend/src/components/LanDetaiels/AppSearch.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="search"></div>
     <section class="hero flow " data-padding="compact">
+      <div class="searchLan">
       <div  id="main">
         <div class="search-container">
           <input
@@ -17,35 +18,43 @@
             class="search-icon"
           />
         </div>
-      </div>
+      </div></div>
     </section>
   </template>
   
   <style scoped>
+  .searchLan {
+  background-color: #D9D9D9;
+  height: 68px;
+
+
+  }
   .search-container {
     position: relative;
-    width: 100%;
-    height: 68px;
+    width: 60%;
     display: flex;
     align-items: center;
-    justify-content: start;
-    background-color: #D9D9D9;
+    justify-content: center;
+    height: 68px;
+
+
   }
   .forsearch {
     border-radius: 0.5rem;
     padding: 0.5rem 1rem 0.5rem 0.5rem;
     outline: none;
     border: #D9D9D9 solid 1px;
-    width: 60%;
+    width: 100%;
     margin: 0.5rem;
     height: 40px;
   }
   
   .search-icon {
     position: absolute;
-    right: 55%;
+    left: 30px;
     top: 50%;
     width: 20px;
     transform: translateY(-50%);
   }
+
   </style>
