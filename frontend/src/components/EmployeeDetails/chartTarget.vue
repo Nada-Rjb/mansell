ef04b0f770129081ea87e7d9696ce5199da1d960
Nada-Rjb <enadaragab2020@gmail.com>
2025-03-21 02:38:06 +0200

    start at chart

--- frontend/src/components/EmployeeDetails/chartTarget.vue
+++ frontend/src/components/EmployeeDetails/chartTarget.vue
@@ -1,7 +1,62 @@
 <template>
-     <section class="hero flow section" data-padding="compact">
+      <section class="hero flow section" data-padding="compact">
       <div class="wrapper" id="main">
-    <div>
-        <h1>chart</h1>
-    </div></div> </section>
+          <div class="chart-container">
+
+<DoughnutChart 
+  :chartData="testData" 
+  :options="{ 
+    cutout: '87%', 
+    responsive: true, 
+    plugins: { 
+      legend: { display: false } 
+      text: 'Total: 205',
+    } 
+ 
+  }" 
+/></div>
+</div>
+</section>
 </template>
+
+<script>
+import { ref } from 'vue';
+import { DoughnutChart } from 'vue-chart-3';
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+
+export default {
+  name: 'ChartComponent',
+  components: { DoughnutChart },
+  setup() {
+    const testData = ref({
+      labels: ['target', 'Remaining'],
+      datasets: [
+        {
+          data: [75, 25], // 75% filled, 25% remaining
+          backgroundColor: ['#009951', '#FFFFFF'], 
+          borderColor: ['#14AE5C', '#14AE5C'], 
+          borderWidth: 1, 
+        },
+      ],
+    });
+
+  
+    return { testData};
+  },
+};
+</script>
+<style scoped>
+.chart-container {
+  width: 172px;
+  height: 172px;
+}
+
+@media (min-width: 600px) {
+  .chart-container {
+    width: 400px; /* Increase size on larger screens */
+    height: 400px;
+  }
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,8 @@ import './index.css'
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
-
+import Donut from 'vue-css-donut-chart';
+import 'vue-css-donut-chart/dist/vcdonut.css';
 import {
   Button,
   Card,
@@ -23,5 +24,6 @@ app.use(resourcesPlugin)
 app.component('Button', Button)
 app.component('Card', Card)
 app.component('Input', Input)
+app.use(Donut);
 
 app.mount('#app')
